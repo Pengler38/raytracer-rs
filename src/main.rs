0abb7b69c783f7c07a0e64bc49d5f7c822656dfa
Pengler38 <70914054+Pengler38@users.noreply.github.com>
2025-01-11 14:35:32 -0500

    Implemented the image equivalent of Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use image::{Rgb, RgbImage};
+
 fn main() {
-    println!("Hello, world!");
+    let mut img = RgbImage::new(256, 256);
+
+    for x in 0..=255 {
+        for y in 0..=255 {
+            img.put_pixel(x as u32, y as u32, hello_world_image(x, y));
+        }
+    }
+    _ = img.save("./test.png");
+}
+
+fn hello_world_image(x: u8, y: u8) -> Rgb<u8> {
+    let y_clamp = if y > x { x } else { y };
+    Rgb([255 - x, y, x - y_clamp])
 }
